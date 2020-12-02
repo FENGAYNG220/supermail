5855c707b6ff82752fe5d385ff43d0110f490c1d
unknown <602559258@qq.com>
2020-12-02 23:14:57 +0800

    NavBar封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
+  <div id='home'>
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <h1>首页</h1>
   </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar"
 export default {
-
+  components:{
+    NavBar
+  }
 }
 </script>
 
 <style>
-
+  .home-nav{
+    background-color:var(--color-tint);
+    color: #fff
+  }
 </style>
